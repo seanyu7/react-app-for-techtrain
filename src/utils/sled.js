258620b6c64486265deeaab3c6916da8d9be5226
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-08-25 00:01:13 +0900

    🐛 modified my typo

--- src/utils/sled.js
+++ src/utils/sled.js
@@ -1,7 +1,7 @@
 export const getAllSledData = (url) => {
   return new Promise((resolve, reject) => {
     fetch(url)
-      .then((res) => res.jason())
+      .then((res) => res.json())
       .then((data) => resolve(data));
      
   });
