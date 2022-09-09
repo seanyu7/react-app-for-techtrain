adce3f7f61d80e46d538632b72c13ff9a07d4fab
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-09-09 23:07:46 +0900

    👽️ changed URL to see the API is working properly

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { getAllSledData } from "./utils/sled";
 
 function App() {
   const initialURL =
-    "http://railway-react-bulletin-board.herokuapp.com";
+    "https://railway-react-bulletin-board.herokuapp.com";
     //this URL cannot activate and cannnot get the info from here.
   const [loading, setLoading] = useState(true);
 
