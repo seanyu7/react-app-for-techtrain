45a703e485b2dbc42a9a667bcf15fd9065676553
seanyu7 <yuinzjumpuphand7@yahoo.co.jp>
2022-10-17 22:21:43 +0900

    deleted and rename a file because of typo

--- src/utils/sled.js
+++
@@ -1,9 +0,0 @@
-export const getAllSledData = (url) => {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => resolve(data));
-     
-  }
-  );
-};
